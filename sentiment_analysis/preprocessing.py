43437ddb703a8d696af9d8c017f8dc46de9d34fd
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-06-18 17:37:10 +0530

    Create preprocessing.py

---
+++ sentiment_analysis/preprocessing.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def preprocess_text(text):
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if not word in stop_words]
+    return " ".join(tokens)
